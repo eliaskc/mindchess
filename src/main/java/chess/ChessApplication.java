f0c56dc8b71b121400835affb391d0a1329de6a9
elias-carlson <carlson.elias@gmail.com>
2020-09-11 11:31:35 +0200

    Setup JavaFX with Maven. Can launch empty window
    Elias C, Erik

--- src/main/java/chess/ChessApplication.java
+++ src/main/java/chess/ChessApplication.java
@@ -6,12 +6,24 @@ package chess;
 //import main.java.chess.controller.ChessController;
 //import main.java.chess.controller.MenuController;
 
-public final class ChessApplication {
-	private ChessApplication() {
-		/* No instances allowed! */
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public final class ChessApplication extends Application {
+	@Override
+	public void start(Stage stage) throws Exception {
+		System.out.println("Hellooo");
+		Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("menu.fxml"));
+		Scene scene = new Scene(root);
+
+		stage.setScene(scene);
+		stage.show();
 	}
 
 	public static void main(String[] args) {
-
+		Application.launch(args);
 	}
 }
