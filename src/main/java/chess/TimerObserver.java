90ca114b377b5546f997d587320174791517632b
ArvidHolmqvist <arvidholmqvist98@gmail.com>
2020-09-24 14:21:33 +0200

    chessTimer implemented in model
    AH EH
    
    Co-Authored-By: Elias Hallberg <54537680+Elias-Hallberg@users.noreply.github.com>

---
+++ src/main/java/chess/TimerObserver.java
@@ -0,0 +1,5 @@
+package chess;
+
+public interface TimerObserver {
+    void updateTimer();
+}
--- src/main/java/chess/model/ChessTimer.java
+++ src/main/java/chess/model/ChessTimer.java
@@ -1,33 +1,67 @@
 package chess.model;
+import chess.TimerObserver;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class ChessTimer {
-    private int time;
+    private int time = 0;
+    private boolean active = false;
     private Timer timer = new Timer();
+    private List<TimerObserver> observers = new ArrayList<>();
 
     public ChessTimer() {
 
     }
 
-
-    // To be discussed before implementation, observer again?
-    public void testSetTime(int time) {
-        System.out.println(time);
-        int i = 0;
+    public void startTimer(int time) {
+        stopTimer();
+        timer = new Timer();
+        setTime(time);
+        System.out.println("timer " + timer + " started");
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                System.out.println(i);
+                if(active)decrementTime();
+                if(getTime() <= 0){
+                    notifyObservers();
+                    stopTimer();
+                }
             }
         }, 0, 1000);
     }
 
-    public void setTime(int time) {
+    public void stopTimer(){
+        timer.cancel();
+        setTime(0);
+        System.out.println("timer " + timer + " stopped");
+        setActive(false);
+    }
+
+    private void decrementTime(){
+        time--;
+        notifyObservers();
+    }
+
+    private void notifyObservers(){
+        for (TimerObserver o: observers) {
+            o.updateTimer();
+        }
+    }
+
+
+    private void setTime(int time) {
         this.time = time;
     }
 
     public int getTime() {
         return time;
+    }
+
+    public void setActive(boolean active) {
+        System.out.println(timer + " active=" + active);
+        this.active = active;
     }
 }
