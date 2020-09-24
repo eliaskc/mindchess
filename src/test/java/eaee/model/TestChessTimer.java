7b76c0b0b01c1bc65ba5f905161ee2455a4aeb68
ArvidHolmqvist <arvidholmqvist98@gmail.com>
2020-09-24 15:53:09 +0200

    chesstimer test skeleton
    AH,EH
    
    Co-Authored-By: Elias Hallberg <54537680+Elias-Hallberg@users.noreply.github.com>

---
+++ src/test/java/eaee/model/TestChessTimer.java
@@ -0,0 +1,14 @@
+package eaee.model;
+
+import chess.controller.ChessController;
+import chess.model.*;
+import chess.model.Piece;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+public class TestChessTimer {
+    
+}
