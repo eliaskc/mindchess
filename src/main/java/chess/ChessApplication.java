20ce10dcb8bff4aa80d26c0764b44c3c86575c1c
elias-carlson <carlson.elias@gmail.com>
2020-10-01 13:53:02 +0200

    Fixed errors in pom.xml
    Also removed unused imports in ChessApplication (media)
    
    EC; EW; EH; AH

--- src/main/java/chess/ChessApplication.java
+++ src/main/java/chess/ChessApplication.java
@@ -6,10 +6,6 @@ import chess.controller.MenuController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 
 public final class ChessApplication extends Application {
