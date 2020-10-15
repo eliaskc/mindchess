de55497dfc7d5639a82a90d5549631184258d001
elias-carlson <carlson.elias@gmail.com>
2020-10-15 17:53:59 +0200

    Analyze game animations finished
    EC, EW,  AH, EH

--- src/main/java/chess/controller/PlyController.java
+++ src/main/java/chess/controller/PlyController.java
@@ -94,31 +94,13 @@ public class PlyController extends AnchorPane {
         movedPiece.setImage(imageHandler.createPieceImage(ply.getMovedPiece().getPieceType(), ply.getMovedPiece().getColor()));
         movedPiece.setFitWidth(40);
         movedPiece.setFitHeight(40);
-        movedPiece.setX(ply.getMovedFrom().x*40);
-        movedPiece.setY(ply.getMovedFrom().y*40);
 
-        ScaleTransition st = new ScaleTransition(Duration.millis(500), movedPiece);
-        st.setFromX(1.0);
-        st.setFromY(1.0);
-        st.setToX(0);
-        st.setToY(0);
-        st.setCycleCount(1);
-
-        st.setOnFinished(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                movedPiece.setX(ply.getMovedTo().x*40);
-                movedPiece.setY(ply.getMovedTo().y*40);
-            }
-        });
-
-        ScaleTransition st2 = new ScaleTransition(Duration.millis(500), movedPiece);
-        st2.setFromX(0);
-        st2.setFromY(0);
-        st2.setToX(1.0);
-        st2.setToY(1.0);
-        st2.setCycleCount(1);
-
+        TranslateTransition tt = new TranslateTransition(Duration.millis(750), movedPiece);
+        tt.setFromX(ply.getMovedFrom().x*40);
+        tt.setFromY(ply.getMovedFrom().y*40);
+        tt.setToX(ply.getMovedTo().x*40);
+        tt.setToY(ply.getMovedTo().y*40);
+        tt.setCycleCount(1);
 
         ImageView attackedPiece = new ImageView();
         if(ply.getBoardSnapshot().containsKey(ply.getMovedTo())){
@@ -129,18 +111,17 @@ public class PlyController extends AnchorPane {
             attackedPiece.setY(ply.getMovedTo().y*40);
         }
 
-        ScaleTransition st3 = new ScaleTransition(Duration.millis(500), attackedPiece);
-        st3.setFromX(1.0);
-        st3.setFromY(1.0);
-        st3.setToX(0);
-        st3.setToY(0);
-        st3.setCycleCount(1);
+        ScaleTransition st = new ScaleTransition(Duration.millis(750), attackedPiece);
+        st.setFromX(1.0);
+        st.setFromY(1.0);
+        st.setToX(0);
+        st.setToY(0);
+        st.setCycleCount(1);
 
-        ParallelTransition pt = new ParallelTransition(st2, st3);
-        SequentialTransition sqt = new SequentialTransition(st, pt);
+        ParallelTransition pt = new ParallelTransition(tt, st);
 
-        sqt.setCycleCount(1);
-        sqt.play();
+        pt.setCycleCount(1);
+        pt.play();
 
         imageViewList.add(movedPiece);
         imageViewList.add(attackedPiece);
