7ce72990178bcb0965e13a20f9a148ea783b98c4
elias-carlson <carlson.elias@gmail.com>
2020-10-19 18:13:49 +0200

    Began reimplementation of castling
    EC, EW

--- src/main/java/chess/model/moveDelegates/KingMoveDelegate.java
+++ src/main/java/chess/model/moveDelegates/KingMoveDelegate.java
@@ -23,6 +23,62 @@ public class KingMoveDelegate implements IMoveDelegate {
         legalPoints.addAll(MovementLogicUtil.downRight(board, pointToCheck, 1));
         legalPoints.addAll(MovementLogicUtil.downLeft(board, pointToCheck, 1));
 
+        legalPoints.addAll(getCastlingPoints(board, pointToCheck, pieceOnPointHasMoved));
+
         return legalPoints;
+    }
+
+    List<Point> getCastlingPoints(Board board, Point pointToCheck, boolean hasMoved) {
+        List<Point> castlingPoints = new ArrayList<>();
+
+        if (!hasMoved) {
+            if (checkRightCastling(board, pointToCheck, hasMoved)) {
+                castlingPoints.add(new Point(pointToCheck.x + 2, pointToCheck.y, true));
+            }
+            if (checkLeftCastling(board, pointToCheck, hasMoved)) {
+                castlingPoints.add(new Point(pointToCheck.x - 2, pointToCheck.y));
+            }
+        }
+        return castlingPoints;
+    }
+
+    /**
+     *
+     * @param board
+     * @param pointToCheck
+     * @param hasMoved
+     * @return
+     */
+    private boolean checkRightCastling(Board board, Point pointToCheck, boolean hasMoved) {
+        for (int i = pointToCheck.x + 1; i <= pointToCheck.x + 2; i++) {
+            if (MovementLogicUtil.isOccupied(board, new Point(i, pointToCheck.y))) {
+                return false;
+            }
+        }
+        Point p = new Point(pointToCheck.x + 3, pointToCheck.y);
+        if (MovementLogicUtil.isOccupied(board, p)) {
+            return board.isPieceOnPointRook(p) && !hasMoved && board.pieceOnPointColorEquals(p, board.fetchPieceOnPointColor(pointToCheck));
+        }
+        return false;
+    }
+
+    /**
+     *
+     * @param board
+     * @param pointToCheck
+     * @param hasMoved
+     * @return
+     */
+    private boolean checkLeftCastling(Board board, Point pointToCheck, boolean hasMoved) {
+        for (int i = pointToCheck.x - 1; i >= pointToCheck.x - 3; i--) {
+            if (MovementLogicUtil.isOccupied(board, new Point(i, pointToCheck.y))) {
+                return false;
+            }
+        }
+        Point p = new Point(pointToCheck.x - 4, pointToCheck.y);
+        if (MovementLogicUtil.isOccupied(board, p)) {
+            return board.isPieceOnPointRook(p) && !hasMoved && board.pieceOnPointColorEquals(p, board.fetchPieceOnPointColor(pointToCheck));
+        }
+        return false;
     }
 }
