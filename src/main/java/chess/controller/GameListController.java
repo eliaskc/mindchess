4c69a64c91e2105041b67a00bcbaafddf0e3051d
Elias Hallberg <elias.j.e.h@hotmail.se>
2020-10-20 16:37:54 +0200

    bugFix
    AH,EH,EC,EW
    
    Co-Authored-By: ArvidHolmqvist <54509607+ArvidHolmqvist@users.noreply.github.com>
    Co-Authored-By: elias-carlson <23519471+elias-carlson@users.noreply.github.com>
    Co-Authored-By: erikwessman <38782157+erikwessman@users.noreply.github.com>

--- src/main/java/chess/controller/GameListController.java
+++ src/main/java/chess/controller/GameListController.java
@@ -4,24 +4,28 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-import java.awt.*;
+
 import java.io.IOException;
 
 public class GameListController extends AnchorPane {
+    @FXML
+    private Label lblGameList;
 
     public GameListController(String player1Name, String player2Name, String gameStatus) {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/gameListView.fxml"));
         fxmlLoader.setRoot(this);
         fxmlLoader.setController(this);
-        
+        //this.lblGameList.setText(String.format("#%d", player1Name + " vs " + player2Name + ": Matchstatus-" + gameStatus));
         try {
             fxmlLoader.load();
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
-        //lblGameLabel.setText(player1Name + " vs " + player2Name + ": Matchstatus-" + gameStatus);
+
+        this.lblGameList.setText(player1Name + " vs " + player2Name + ": Matchstatus-" + gameStatus);
     }
 }
