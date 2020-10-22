0ba8f54e44868afa8dba6494f5cb9a2699e36ebb
Elias Hallberg <elias.j.e.h@hotmail.se>
2020-10-22 17:25:16 +0200

    nameChange
    EH,AH
    
    Co-Authored-By: ArvidHolmqvist <54509607+ArvidHolmqvist@users.noreply.github.com>

--- src/test/java/mindchess/model/TestMoveDelegates.java
+++ src/test/java/mindchess/model/TestMoveDelegates.java
@@ -11,7 +11,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class TestMovementLogicUtil {
+public class TestMoveDelegates {
     ChessFacade model;
     Board board;
 
